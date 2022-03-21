258e55fbd8252b1b54a0ffd58cee6c6c79418d6d
Magilo-Ayuen <magiloayuen@gmail.com>
2022-03-21 18:30:16 +0300

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code (key part of the program)
+ *
+ * Return: Always (0)
+ */
+void reset(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a Directory for Pointers, Arrays, and Strings
