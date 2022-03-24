204ff86504c90c663ad6cec7b29b7e636527364f
Magilo-Ayuen <magiloayuen@gmail.com>
2022-03-25 02:04:20 +0300

    More pointers, arrays and Strings= 5-String_Toupper.c REFIX

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
-/**
- * *string_toupper - changes all lowercase letters of a string to upper.
- *
- * @str: string to return.
- * Return: str
- */
-char *string_toupper(char *str)
-{
-	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+/**
+ * *string_toupper - converts a string to uppercase
+ * @s: string to be converted to uppercase
+ *
+ * Return: converted string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
 	}
-	return (str);
+	return (s);
 }
