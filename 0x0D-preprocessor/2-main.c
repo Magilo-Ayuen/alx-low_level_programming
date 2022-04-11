289bc419ae0975792850e0a205bb44a6ddf34b5e
Magilo-Ayuen <magiloayuen@gmail.com>
2022-04-11 23:48:54 +0300

    Pre-processor - 2-main.c(REFIX)

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-  * main - prints the file
-  * return: 0 always for a success
-  */
+ * main - prints the file
+ * return: 0 always for a success
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
