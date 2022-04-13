3a4cdf0155cf01d50474ee11196adc5efb20bd3e
Magilo-Ayuen <magiloayuen@gmail.com>
2022-04-14 02:44:50 +0300

    Function pointers - REFIX

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,8 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
- *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
+ * get_op_func - selects the correct operation function asked by user
+ * @s: operator passed as arg
+ * Return: function pointer corresponding to operator passed
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -16,13 +12,17 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);      /* selected op func() */
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
 }
